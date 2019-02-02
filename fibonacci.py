4770b9ae17591f8f3a8638a38838e4b42766e3e6
Shihui Tan <shihuitan@Shihuis-MacBook-Pro-2.local>
2019-02-02 16:51:56 +0900

    Improve fib function

--- fibonacci.py
+++ fibonacci.py
@@ -4,16 +4,18 @@
 ###############
 def fib(n):
     """ Fibonacci sequence """
-    if n <= 0:
-        return('Please input positive integer.')
-    else:
-        seq = []
-        for k in range(n):
-            if k <= 1:
-                seq.append(k)
-            else:
-                seq.append(sum(seq[k-2:k]))
+    if n == 1 :
+        return([0])
+        print('The first ', n, ' numbers of the Fibonacci sequence is : [0]')
+    elif n == 2:
+        return([0,1])
+        print('The first ', n, ' numbers of the Fibonacci sequence is : [0, 1]')
+    elif n > 2:
+        seq = [0,1] 
+        for k in range(n-2):
+            seq.append(sum(seq[k:k+2]))
         print('The first ', n, ' numbers of the Fibonacci sequence is :', seq)
+        return(seq)
 
 #######################
 if __name__ == "__main__":
