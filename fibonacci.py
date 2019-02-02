41ceb9fd4fecb4e8ffc2828b96fdb6c8e28db17b
Shihui Tan <shihuitan@Shihuis-MacBook-Pro-2.local>
2019-02-02 11:25:36 +0900

    fib sequence

---
+++ fibonacci.py
@@ -0,0 +1,27 @@
+###############
+## Fibonacci ##
+## 2/2/2019  ##
+###############
+def fib(n):
+    """ Fibonacci sequence """
+    if n <= 0:
+        return('Please input number greater than 1.')
+    else:
+        i_1 = 0
+        i = 1
+        seq = [i_1,i]
+        while len(seq) <= n:
+             i_2 = i_1
+             i_1 = i
+             i = i_2 + i_1
+             seq.append(i)
+        print('Fibnoacci sequence with n =', n, 'is :', seq)
+
+#######################
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please state n to compute fibonacci sequence.")
+    else:
+        n = int(sys.argv[1])
+        fib(n)
