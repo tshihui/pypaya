317e0a4dc37cc6631d6a8b25651511e56cd5e7c8
Shihui Tan <shihuitan@Shihuis-MacBook-Pro-2.local>
2019-05-26 21:31:47 +0900

    Introducing Python

---
+++ introducingPython/chapter3.py
@@ -0,0 +1,34 @@
+# 3.1 
+years_list = [1990, 1991, 1992, 1993, 1994, 1995]
+
+# 3.2
+years_list[3]
+
+# 3.3
+years_list[-1]
+
+# 3.4
+things = ['mozzarella', 'cinderella', 'salmonella']
+
+# 3.5
+things[1].title()
+# no
+
+# 3.6
+things[0].upper()
+
+# 3.7
+del things[2]
+print(things)
+
+# 3.8
+surprise = ['Groucho', 'Chico', 'Harpo']
+
+# 3.9
+llast = surprise[2].lower()
+print(llast[::-1].upper())
+
+# 3.10
+e2f = dict([['dog','chien'], ['cat', 'chat'], ['walrus', 'morse']])
+
+
