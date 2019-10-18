fe88a353a49b5dcd1f4188c6e76c09b4a594790b
Shi Hui Tan <shihuit.14@gmail.com>
2019-10-18 11:39:48 +0800

    Codility Lesson 4 revised

--- codility_practices/maxCounters.py
+++ codility_practices/maxCounters.py
@@ -2,8 +2,29 @@
 ## Codility Lesson 4: MaxCounters.py
 #####################################
 
-#--- Attempt 1: ---#
+#--- Attempt 2 ---#
+from collections import defaultdict
+
 def solution(N, A):
+    L = defaultdict(int)
+    maxV = 0
+    for k in A:
+        if k != N+1:
+            L[k] += 1
+        else:
+            if (len(L.values()) > 0):
+                maxV = max(L.values())
+            L = defaultdict(lambda : maxV)
+
+    output = []
+    for x in range(1, N+1):
+        output.append(L[x])
+    
+    return(output)
+
+
+#--- Attempt 1 ---#
+def solutionOld(N, A):
     L = [0] * N
     for k in A:
         print(k)
